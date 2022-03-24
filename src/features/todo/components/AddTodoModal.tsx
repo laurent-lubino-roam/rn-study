012f616469afb6001a1c4344bc25a1a840893846
Laurent Lubino <laurent.lubino@roamdigital.com>
2022-03-25 09:48:18 +1300

    feat: move background color to safearea

--- src/features/todo/components/AddTodoModal.tsx
+++ src/features/todo/components/AddTodoModal.tsx
@@ -68,9 +68,9 @@ export default AddTodoModal;
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
   background: {
-    backgroundColor: 'rgba(0,0,0,0.2)',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
